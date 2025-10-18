bc47fda6d6467eb2e5ee54722ac306c246e1ac86
AsfarButt <asfarbutt69@gmail.com>
2025-10-18 19:14:20 +0500

    Initial Update

---
+++ src/app/fixedblock.tsx
@@ -0,0 +1,23 @@
+
+
+
+export default function FixedBlock(){
+
+
+    return(<div className="fixed flex w-screen h-screen">
+        <div className="w-[8%] min-w-26 h-[92vh] flex justify-center border-r-2 border-black border-box">
+        <div className="relative mt-2 flex flex-col">
+            <img src="/lines.png" className="w-10 h-10" alt="Lines PNG" />
+            <img src="/search.png" className="w-10 h-10" alt="Search PNG" />
+        </div>
+        </div>
+        <div className="relative w-full flex justify-between bg-black/20">
+        <div className="text-2xl m-4"><h1>SARASHOTS</h1><p className="text-sm">A true Film maker</p></div>
+        <div className="relative m-4 w-32 h-10 flex justify-center items-center text-semibold text-white bg-orange-700/90">BUY NOW</div>
+        </div>
+    </div>)
+}
+
+
+// Start from here and work your way up it isnt rocket science nor is it that complex so try (just do) do it in 3 max 4 days
+
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import FixedBlock from "./fixedblock";
+
+
+export default function Page(){
+
+
+  return(<div className="relative w-screen h-screen">
+    
+    <FixedBlock />
+    
+     </div>)
+}
