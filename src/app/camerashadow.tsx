c97a388260ae66dbdbc241ea02e96f31716e32ac
AsfarButt <asfarbutt69@gmail.com>
2025-10-19 15:43:36 +0500

    Update 1

---
+++ src/app/camerashadow.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React,{useRef, useEffect} from "react";
+
+
+export default function CameraShadow(){
+    const camera = useRef<HTMLDivElement | null>(null);
+
+    useEffect(()=>{
+        camera.current?.classList.add("rotate-z-25","scale-70")
+    },[])
+
+
+    return(<div className="fixed w-screen h-screen flex justify-center items-center z-0 overflow-hidden">
+        <div className="relative w-[90%] max-w-260 h-140 flex justify-center items-center transition-tranform duration-1500" ref={camera}>
+            {/* <img src="/camera2.png" alt="Main Camera" className="relative w-full h-full" /> */}
+             <div className="absolute bottom-0 left-0 w-[70%] h-[60%] bg-black/50 blur-xl mt-5 [box-shadow:0_0_80px_20px_rgba(0,0,0,0.9)]" />
+        </div>
+
+    </div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 import FixedBlock from "./fixedblock";
-
+import Camera from "./camera";
+import ScrollablePart from "./scrollablepart";
+import CameraShadow from "./camerashadow";
 
 export default function Page(){
 
+  function ScrollEventFunc(e: React.UIEvent<HTMLDivElement>){
+    console.log(e.currentTarget.scrollTop);
+  }
 
-  return(<div className="relative w-screen h-screen">
+
+  return(<div className="relative w-screen h-fit bg-orange-100/80 overflow-hidden" onScroll={(e) => ScrollEventFunc(e)}>
     
     <FixedBlock />
+    <Camera />
+    <CameraShadow />
+    <ScrollablePart />
     
      </div>)
 }
